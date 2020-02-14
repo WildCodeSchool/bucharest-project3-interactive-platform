f3776eee4634282a4000a42b2442dc7593edcd27
S0nicTheHedgehog <notyorphobia@gmail.com>
2020-02-14 17:10:07 +0200

    resolved

--- frontend/src/components/homepage/homepage-components/navbar/NavBar.js
+++ frontend/src/components/homepage/homepage-components/navbar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter, Link } from "react-router-dom";
-import { Navbar, Nav, Button, Row, Col,Container } from 'react-bootstrap';
+import { Navbar, Nav, Button, Row, Col, Container } from 'react-bootstrap';
 import logo from "../../../../assets/img/logo_techir.png";
 import './NavBar.css'
 import Login from '../login/Login';
@@ -9,6 +9,8 @@ import SignUp from "../signup/SignUp";
 
 import { connect } from 'react-redux';
 
+
+const Admin = (props) => <div className='admin'><Link to='/admin'>Admin</Link></div>
 
 
 class Navigation extends React.Component {
@@ -46,14 +48,11 @@ class Navigation extends React.Component {
     }
 
     toggleLogin = () => {
-        console.log('do');
         this.loginCounter += 1;
         if (this.loginCounter % 2 === 0) {
             this.props.onClickLogin(false);
-            console.log('open');
         } else if (this.counter % 2 !== 0) {
             this.props.onClickLogin(true);
-            console.log('close');
         }
     }
 
@@ -64,13 +63,11 @@ class Navigation extends React.Component {
 
     }
     componentDidMount() {
-        console.log(this.props.user);
+        console.log(this.props);
 
 
     }
     render() {
-
-        // console.log(this.props)
         if (this.props.redirectLogin && this.state.isShownSignup) this.setState({
             isShownLogin: !this.state.isShownLogin,
             isShownSignup: false,
@@ -88,45 +85,46 @@ class Navigation extends React.Component {
         }
 
         return (
-            <Container fluid style={{margin: 0, padding: 0}}>
-                <Row noGutters style={{margin: 0, padding: 0}}>
-                <Col>
-                <Navbar bg="dark" expand="lg" className="nav">
-                    <Navbar.Brand >
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-                    {!this.props.isUserLogged ?
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className=" nav-items">
-                                <div className="nav-link one" onClick={this.showLogIn}> Conectare</div>
-                                <div className="nav-link two" onClick={this.showSignUp}> Creeaza cont</div>
-                            </Nav>
-                        </Navbar.Collapse> :
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className=" nav-items">
-                                <Link className="nav-link one" onClick={this.disconnectUser}>Deconectare</Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    }
-                </Navbar>
-                </Col>
-                </Row>
-                <Row noGutters style={{margin: 0, padding: 0}}>
+            <Container fluid style={{ margin: 0, padding: 0 }}>
+                <Row noGutters style={{ margin: 0, padding: 0 }}>
                     <Col>
-                <div className="logo-div">
+                        <Navbar bg="dark" expand="lg" className="nav">
+                            <Navbar.Brand >
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                    <Link to="/"><img src="http://www.techir.ro/wp-content/uploads/2015/03/logo_techir.png" width="274px" height="105px" className="logo-q" /></Link>
-                </div>
-                </Col>
+                            {!this.props.isUserLogged ?
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className=" nav-items">
+                                        <div className="nav-link one" onClick={this.showLogIn}> Conectare</div>
+                                        <div className="nav-link two" onClick={this.showSignUp}> Creeaza cont</div>
+                                    </Nav>
+                                </Navbar.Collapse> :
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className=" nav-items">
+                                        <Link className="nav-link one" onClick={this.disconnectUser}>Deconectare</Link>
+                                        {this.props.user && this.props.user.acces_level && < Admin />}
+                                    </Nav>
+                                </Navbar.Collapse>
+                            }
+                        </Navbar>
+                    </Col>
                 </Row>
-                <Row noGutters style={{margin: 0, padding: 0}}>
+                <Row noGutters style={{ margin: 0, padding: 0 }}>
+                    <Col>
+                        <div className="logo-div">
+
+                            <Link to="/"><img src="http://www.techir.ro/wp-content/uploads/2015/03/logo_techir.png" width="274px" height="105px" className="logo-q" /></Link>
+                        </div>
+                    </Col>
+                </Row>
+                <Row noGutters style={{ margin: 0, padding: 0 }}>
                     <Col xs={6} sm={6} md={4} lg={4}>
-                {!this.props.isUserLogged && this.state.isShownLogin && !this.state.isShownSignup ? <Login /> : null}
-                {!this.props.isUserLogged && this.state.isShownSignup && !this.state.isShownLogin ? <SignUp /> : null}
-                </Col>
+                        {!this.props.isUserLogged && this.state.isShownLogin && !this.state.isShownSignup ? <Login /> : null}
+                        {!this.props.isUserLogged && this.state.isShownSignup && !this.state.isShownLogin ? <SignUp /> : null}
+                    </Col>
                 </Row>
-               </Container>
+            </Container>
         );
     }
 }
@@ -134,7 +132,7 @@ class Navigation extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        // user: state.authentication.user,
+        user: state.authentication.user,
         token: state.authentication.token,
         msg: state.authentication.msg,
         isUserLogged: state.authentication.isUserLogged,
