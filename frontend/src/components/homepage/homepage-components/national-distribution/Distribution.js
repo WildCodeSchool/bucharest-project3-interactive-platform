cb4b592f051837fdace9a453eda0488b8bccc057
rriamarria <fmariamarinescu@gmail.com>
2020-02-15 12:04:05 +0200

    creates Distribution.component

--- frontend/src/components/homepage/homepage-components/national-distribution/Distribution.js
+++ frontend/src/components/homepage/homepage-components/national-distribution/Distribution.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
 class Distribution extends React.Component {
-    constructor(props) {}
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <Container fluid>
+<Row>
+            <div>
+                <h3>Distributie nationala</h3>
+                <div className="national">
+                    <img src=""></img>
+                    <img src=""></img>
+                    <img src=""></img>
+                </div>
+                <p>Produsele Techir sunt distribuite national prin intermediul lanturilor de farmacii si magazinelor Catena, DM, Mega Image si Carturesti.</p>
+                <p>In aceste locatii gasesti doar o selectie de produse din gamele Techir</p>
+            </div>
+            </Row>
+            </Container>
+        )
+    }
 }
+
+
+
+export default Distribution;
